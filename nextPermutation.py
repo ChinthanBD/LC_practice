7bf5593ee17ca62fa0f39dca4f4769cc09d9572c
cdevaraju <cdevaraju@sandvine.com>
2024-02-04 23:05:36 +0530

    Pascal Triangle, MaxSubarraySum, NextPermutation

--- nextPermutation.py
+++ nextPermutation.py
@@ -1,22 +1,48 @@
+# https://leetcode.com/problems/next-permutation/
+# a = [2, 1, 5, 4, 3, 0, 0]
 
-a = [2, 1, 5, 4, 3, 0, 0]
+# dip = None
+# for i in range(len(a) - 2, -1, -1):
+#     print(i)
+#     if a[i] < a[i + 1]:
+#         dip = i
+#         break
 
-dip = None
-for i in range(len(a) - 2, -1, -1):
-    print(i)
-    if a[i] < a[i + 1]:
-        dip = i
-        break
+# if dip is not None:
+#     for i in range(len(a) - 1, dip, -1):
+#         if a[i] > a[dip]:
+#             a[i], a[dip] = a[dip], a[i]
+#             break
 
-if dip is not None:
-    for i in range(len(a) - 1, dip, -1):
-        if a[i] > a[dip]:
-            a[i], a[dip] = a[dip], a[i]
-            break
-
-a = a[:dip + 1] + a[dip + 1:][::-1]
-print(a)
+# a = a[:dip + 1] + a[dip + 1:][::-1]
+# print(a)
 
 
 
+        
+
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        dip = -1
+        for i in range(n-2, -1, -1):
+            if nums[i] < nums[i+1]:
+                dip = i
+                break
+
+        if dip ==-1:
+            return nums.sort()
+    
+        for i in range(n-1, dip, -1):
+            if nums[i] > nums[dip]:
+                nums[i], nums[dip] = nums[dip], nums[i]
+                break
+            # Reverse the suffix
+        nums[dip+1:] = reversed(nums[dip+1:])
+
+        return nums
         
---
+++ pascalTriangle.py
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/pascals-triangle/
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        if numRows == 0:
+            return []
+        
+        # Initialize the result with the first row
+        result = [[1]]
+        
+        for i in range(1, numRows):
+            row = [1]  # First element of each row is always 1
+            for j in range(1, i):  # Calculate the elements in between
+                row.append(result[i-1][j-1] + result[i-1][j])
+            row.append(1)  # Last element of each row is always 1
+            result.append(row)
+        
+        return result
+
+
+##########################
+    #def generate(self, numRows: int) -> List[List[int]]:
+    #    result = []
+    #    for row in range(1, numRows + 1):
+    #        current_row = []
+    #        for column in range(1, row + 1):
+    #            # Calculate the element using the combination formula
+    #            element = self.combination(row - 1, column - 1)
+    #            current_row.append(element)
+    #        result.append(current_row)
+    #    return result
+    #
+    #def combination(self, n: int, k: int) -> int:
+    #    # Calculate n choose k
+    #    numerator = 1
+    #    denominator = 1
+    #    for i in range(k):
+    #        numerator *= n - i
+    #        denominator *= i + 1
+    #    return numerator // denominator
