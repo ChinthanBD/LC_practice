583aafdb312534541a213d976d590827b805c7cc
ChinthanBD <chinthan.bd@gmail.com>
2024-02-16 22:52:36 +0530

    candyProblem

---
+++ candy.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/candy/?envType=study-plan-v2&envId=top-interview-150
+from typing import List
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        data = sorted((x,i) for i,x in enumerate(ratings))
+
+        candy = [1] * len(ratings)
+
+        for _, i in data:
+            if i > 0 and ratings[i] > ratings[i - 1]:
+                candy[i] = max(candy[i], candy[i - 1] + 1)
+
+            if i < len(ratings) - 1 and ratings[i] > ratings[i + 1]:
+                candy[i] = max(candy[i], candy[i + 1] + 1)
+
+        return sum(candy)
