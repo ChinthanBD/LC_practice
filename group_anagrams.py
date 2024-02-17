1c10e015855f9ade2ae858c6dd9d68382c10ef19
ChinthanBD <chinthan.bd@gmail.com>
2024-02-17 17:22:35 +0530

    LC Problem solution

---
+++ group_anagrams.py
@@ -0,0 +1,18 @@
+from typing import List
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = []
+        anagram_dict = defaultdict(list)
+
+        for s in strs:
+            
+            sorted_string = ''.join(sorted(s))
+            print(sorted_string)
+            anagram_dict[sorted_string].append(s)
+
+        for string_list in anagram_dict.values():
+            res.append(string_list)
+
+        
+        return res
