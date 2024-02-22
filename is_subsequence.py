7307c9d04e911ba74a593165eef3a6599d7ba302
ChinthanBD <chinthan.bd@gmail.com>
2024-02-22 23:38:52 +0530

    Two pointer

---
+++ is_subsequence.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/is-subsequence/description/?envType=study-plan-v2&envId=top-interview-150
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        l = 0
+        r = 0
+        while l<len(s) and r<len(t):
+            if s[l] == t[r]:
+                l +=1
+            r+=1
+
+        if l<len(s):
+            return False
+        return True
