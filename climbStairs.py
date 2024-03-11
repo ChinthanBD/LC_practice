bde260fe66c3126e73957c1263226b21ace8c54a
Chinthan <chinthan.bd@gmail.com>
2024-03-11 23:06:04 +0530

    DPP

---
+++ climbStairs.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/climbing-stairs/description/
+# pre-compute
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[0] = dp[1] = 1
+
+        # Calculate the number of distinct ways for each step using dynamic programming
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
---
+++ trailingZeroesInFactorial.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/factorial-trailing-zeroes/
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        res = 0
+        while n>0:
+            n = n//5
+            res += n
+        return res
+        
