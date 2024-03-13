9e3956f4fb90c2f1dc1c1c1779d5f2fffe67cc04
Chinthan <chinthan.bd@gmail.com>
2024-03-13 22:16:49 +0530

    n frog jump space optimization

--- nForgJump.py
+++ nForgJump.py
@@ -9,9 +9,7 @@ from typing import *
   
 def frogJump(n: int, heights: List[int]) -> int:
     dp = [None] * n
-    
-
-
+    # memoization
     def func(step, dp, heights):
         if step == 0:
             return 0
@@ -28,3 +26,36 @@ def frogJump(n: int, heights: List[int]) -> int:
         return dp[step]
 
     return func(n-1, dp, heights)
+
+##########################
+
+def frogJump(n: int, heights: List[int]) -> int:
+
+    if n <= 1:
+        return 0 
+
+    dp = [0] * n  
+    dp[1] = abs(heights[1] - heights[0])  
+    for i in range(2, n):
+        jump1 = dp[i-1] + abs(heights[i] - heights[i-1])  
+        jump2 = dp[i-2] + abs(heights[i] - heights[i-2])  
+        dp[i] = min(jump1, jump2)  
+    
+    return dp[n-1]
+
+##########################
+def frogJump(n: int, heights: List[int]) -> int:
+    prev = 0 
+    prev2 = 0
+    for i in range(1,n):
+        fs = prev + abs(heights[i]-heights[i-1])
+        ss = float("inf")
+
+        if i >1:
+            ss = prev2 + abs(heights[i]-heights[i-2])
+        
+        curri = min(fs,ss)
+        prev2 = prev
+        prev = curri
+    
+    return prev
