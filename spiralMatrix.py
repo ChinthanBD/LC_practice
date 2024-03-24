3dd4ecc1ec417d019765717d3d0b82196bd372b2
Chinthan <chinthan.bd@gmail.com>
2024-03-24 23:04:45 +0530

    modified spiral matrix

--- spiralMatrix.py
+++ spiralMatrix.py
@@ -4,26 +4,27 @@ class Solution:
             return []
 
         rows, cols = len(matrix), len(matrix[0])
-        top, bottom, left, right = 0, rows-1, 0, cols-1
+        top, bottom, left, right = 0, rows, 0, cols
         result = []
         
-        while len(result) < rows * cols:
-            for i in range(left, right+1):
+        while left < right and top < bottom:
+            for i in range(left, right):
                 result.append(matrix[top][i])
             top += 1
             
-            for i in range(top, bottom+1):
-                result.append(matrix[i][right])
+            for i in range(top, bottom):
+                result.append(matrix[i][right-1])
             right -= 1
             
-            if top <= bottom:
-                for i in range(right, left-1, -1):
-                    result.append(matrix[bottom][i])
-                bottom -= 1
+            if not (left < right and top < bottom):
+                break
+
+            for i in range(right-1, left - 1, -1):
+                result.append(matrix[bottom-1][i])
+            bottom -= 1
             
-            if left <= right:
-                for i in range(bottom, top-1, -1):
-                    result.append(matrix[i][left])
-                left += 1
+            for i in range(bottom-1, top - 1, -1):
+                result.append(matrix[i][left])
+            left += 1
         
         return result
