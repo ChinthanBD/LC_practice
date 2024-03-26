f5396e9c935c4acc6c9c096c61133f126892fb0d
Chinthan <chinthan.bd@gmail.com>
2024-03-26 22:43:15 +0530

    Rotate Matrix

---
+++ rotateMatrix.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/rotate-image/?envType=study-plan-v2&envId=top-interview-150
+from typing import List
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        res = [[0] * m for _ in range(n)]
+        for i in range(m):
+            for j in range(n):
+                ele = matrix[i][j]
+
+                dest_i,dest_j = j, m - 1 - i
+                res[dest_i][dest_j] = ele
+        
+        for i in range(m):
+            for j in range(n):  
+                matrix[i][j] = res[i][j]
+                
+### Above is using extra array, Not so good :(                 
