2df5a4e1c922379fae661a247cca78ec41ac606d
Chinthan <chinthan.bd@gmail.com>
2024-04-23 22:14:47 +0530

    binaryTreeFromInorderAndPostorder

--- binaryTreeFromPreOrderAndInOrderTraversal.py
+++ binaryTreeFromPreOrderAndInOrderTraversal.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/?envType=study-plan-v2&envId=top-interview-150
 from typing import List, Optional
 # Definition for a binary tree node.
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -12,9 +13,17 @@ class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         if not preorder or not inorder:
             return None
-
+        
         root = TreeNode(preorder[0])
         mid = inorder.index(preorder[0])
-        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
-        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+        
+        # Calculate the size of the left subtree
+        left_subtree_size = mid
+        
+        # Recursive call to construct the left subtree
+        root.left = self.buildTree(preorder[1:1+left_subtree_size], inorder[:mid])
+        
+        # Recursive call to construct the right subtree
+        root.right = self.buildTree(preorder[1+left_subtree_size:], inorder[mid+1:])
+        
         return root
