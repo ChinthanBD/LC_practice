5352d3ad6c2ea0036186833e13d44c45b72bc8d8
Chinthan <chinthan.bd@gmail.com>
2024-04-24 23:31:05 +0530

    populatingNextRightPointersInEachNode

--- binaryTreeFromPreOrderAndInOrderTraversal.py
+++ binaryTreeFromPreOrderAndInOrderTraversal.py
@@ -1,7 +1,6 @@
 # https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/?envType=study-plan-v2&envId=top-interview-150
 from typing import List, Optional
 # Definition for a binary tree node.
-# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
