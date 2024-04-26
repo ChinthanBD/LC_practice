5801c73350006dcf17571da14e1340c948003395
Chinthan <chinthan.bd@gmail.com>
2024-04-26 22:27:04 +0530

    added file for PopulatingNextRightPointersinEachNodeII problem

---
+++ dfs.py
@@ -0,0 +1,21 @@
+# https://www.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1
+
+#User function Template for python3
+
+class Solution:
+    
+    #Function to return a list containing the DFS traversal of the graph.
+    def dfsOfGraph(self, V, adj):
+        # code here
+        visited = set()
+        traversal = []
+        def dfs(vertex):
+            if vertex not in visited:
+                visited.add(vertex)
+                traversal.append(vertex)
+                for neighbour in adj[vertex]:
+                    if neighbour not in visited:
+                        dfs(neighbour)
+        dfs(0)
+            
+        return traversal
