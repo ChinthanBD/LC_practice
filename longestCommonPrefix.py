ab22820dd3476ec82fc20f68a0a5004c9f7a6959
Chinthan <chinthan.bd@gmail.com>
2024-05-06 22:25:06 +0530

    modified the code

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -2,12 +2,32 @@
 from typing import List
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        prefix = strs[0]
+        # prefix = strs[0]
 
-        for s in strs[1:]:
-            while s.find(prefix) != 0:
-                prefix = prefix[:-1]
+        # for s in strs[1:]:
+        #     while s.find(prefix) != 0:
+        #         prefix = prefix[:-1]
 
-            if not prefix:
-                return ""
-        return prefix
+        #     if not prefix:
+        #         return ""
+        # return prefix
+
+        ###################
+        # res = ''
+        # for i in range(len(strs[0])):
+        #     for s in strs[1:]:
+        #         if i == len(s) or s[i]!= strs[0][i]:
+        #             return res
+                
+        #     res+= strs[0][i]
+        # return res
+
+        #################
+        if len(strs) ==1 or not strs:
+            return strs[0]
+
+        min_str, max_str = min(strs), max(strs)
+        for i in range(len(min_str)):
+            if min_str[i] != max_str[i]:
+                return min_str[:i]
+        return min_str
