4749f80899b1070619cf4e110a4c5551ff485bbf
ChinthanBD <73706088+ChinthanBD@users.noreply.github.com>
2024-05-16 23:06:37 +0530

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -7,7 +7,7 @@ class Solution:
     #Function to return a list containing the DFS traversal of the graph.
     def dfsOfGraph(self, V, adj):
         # code here
-        visited = set()
+        visited = set() # maintain visisted nodes 
         traversal = []
         def dfs(vertex):
             if vertex not in visited:
