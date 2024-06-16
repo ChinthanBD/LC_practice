7a87364aa24d6828e167475fce78674082d95652
Chinthan <chinthan.bd@gmail.com>
2024-06-16 16:29:00 +0530

    Floyd's algorithm

--- findTheDuplicateNumber.py
+++ findTheDuplicateNumber.py
@@ -3,13 +3,13 @@ from typing import List
 from collections import Counter
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        mpp = {}
+        # mpp = {}
 
-        for i in nums:
-            if i in mpp:
-                return i
-            else:
-                mpp[i] = 1
+        # for i in nums:
+        #     if i in mpp:
+        #         return i
+        #     else:
+        #         mpp[i] = 1
 
         ########################################
         ## below less efficient (o(nLog(n))+o(N))
@@ -18,3 +18,23 @@ class Solution:
         # for i in range(len(nums)-1):
         #     if nums[i] == nums[i+1]:
         #         return nums[i]
+
+        ############# Floyd's Algo(Linked list approach)
+
+        fast = slow = nums[0]
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if fast == slow:
+                break
+        
+        slow2 = nums[0]
+
+        while slow != slow2:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+        
+        return slow
+
