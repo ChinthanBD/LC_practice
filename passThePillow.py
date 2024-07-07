5a7475f64f5af5543f00c619a003019938f4d944
Chinthan <chinthan.bd@gmail.com>
2024-07-07 20:29:04 +0530

    pass the pillow

---
+++ passThePillow.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/pass-the-pillow/submissions/1312969986/?envType=daily-question&envId=2024-07-06
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
+        cycle_length = 2 * (n - 1)
+        time = time % cycle_length
+        
+        if time < n:
+            return time + 1
+        else:
+            return 2 * n - time - 1
