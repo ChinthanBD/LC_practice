defd08d0599920f6094297b96ac63b0c6f908b08
ChinthanBD <73706088+ChinthanBD@users.noreply.github.com>
2024-08-11 23:05:21 +0530

    Update validAnagram.py

--- validAnagram.py
+++ validAnagram.py
@@ -1,9 +1,24 @@
 # https://leetcode.com/problems/valid-anagram/description/?envType=study-plan-v2&envId=top-interview-150
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_mpp = Counter(s)
-        t_mpp = Counter(t)
+        # s_mpp = Counter(s)
+        # t_mpp = Counter(t)
 
-        if s_mpp == t_mpp:
-            return True
-        return False
+        # return s_mpp == t_mpp
+
+        ###############
+        if len(s) != len(t):
+            return False
+
+        count = [0] * 26
+
+        for i in range(len(s)):
+            count[ord(t[i])- ord('a')] -=1
+            count[ord(s[i])- ord('a')] +=1
+
+        for i in range(len(count)):
+
+            if count[i] !=0:
+                return False
+        
+        return True
