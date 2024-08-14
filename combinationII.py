0dd71df4c59450fc2f30cc8e1589c6e8a65f35d3
ChinthanBD <73706088+ChinthanBD@users.noreply.github.com>
2024-08-14 13:32:54 +0530

    Update combinationII.py

--- combinationII.py
+++ combinationII.py
@@ -23,3 +23,35 @@ class Solution:
 
         combination(target, 0, [])
         return ans
+
+
+
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()  # Sort candidates to handle duplicates
+
+        ans = []
+        def dfs(i, curr_sum, ele_list):
+            if curr_sum == target:
+                ans.append(ele_list.copy())
+                return
+            
+            if (curr_sum > target or i >= len(candidates)):
+                return 
+            
+            # include current index and call for next index
+            ele_list.append(candidates[i])
+            dfs(i+1, curr_sum + candidates[i], ele_list)
+            ele_list.pop()
+
+            # exclude current index, skip duplicates and call for next index
+            while i < len(candidates) - 1 and candidates[i] == candidates[i + 1]:
+                i += 1
+            
+            dfs(i+1, curr_sum, ele_list)
+
+        
+        dfs(0, 0, [])
+
+        return ans
+
