ee46f99bff665c129e9411a93bfda78e6e04bd78
ChinthanBD <73706088+ChinthanBD@users.noreply.github.com>
2024-09-05 23:14:36 +0530

    Update binarySearch to handle int overflow

--- binarySearch.py
+++ binarySearch.py
@@ -5,7 +5,7 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         l, r = 0, len(nums) -1
         while l<=r:
-            mid = (l+r)//2
+            mid = l + ((r-l)//2)
             curr_ele = nums[mid]
             if curr_ele == target:
                 return mid
