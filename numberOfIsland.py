963e2f389d89f3bb39bb9ae0f92788fd7348e468
ChinthanBD <73706088+ChinthanBD@users.noreply.github.com>
2025-01-09 17:55:50 +0530

    Update numberOfIsland.py

--- numberOfIsland.py
+++ numberOfIsland.py
@@ -33,3 +33,35 @@ class Solution:
                     self.bfs(i, j, m, n, grid)
         
         return ans
+
+
+# https://leetcode.com/problems/number-of-islands/
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        visited = set()
+        row_len = len(grid)
+        col_len = len(grid[0])
+        def bfs(row, col):
+            q = deque([(row,col)])
+            delta = [(0,1), (1,0), (-1,0), (0,-1)]
+            visited.add((row,col))
+
+            while q:
+                r, c = q.popleft()
+                for dr, dc in delta:
+                    nr, nc = r+dr, c+dc
+
+                    if 0<=nr<row_len and 0<=nr and 0<=nc<col_len and grid[nr][nc] == '1' and (nr, nc) not in visited:
+                        visited.add((nr, nc))
+                        q.append((nr, nc))
+
+
+        no_of_islands = 0
+        for i in range(row_len):
+            for j in range(col_len):
+                if grid[i][j] == '1' and (i, j) not in visited: 
+                    bfs(i, j)
+                    no_of_islands +=1
+
+        return no_of_islands
+
